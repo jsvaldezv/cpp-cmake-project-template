ebd5e82055657b0595f9183c585a5a338172126b
Jesús Valdez <jsvaldezv@gmail.com>
2024-06-14 23:32:41 -0600

    Update main file

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include "GUI/MainComponent.h"
 
 int main()
 {
     std::cout << "Hello World from C++ Project Template!\n";
-
-    MainComponent mainComponent;
 
     return 0;
 }
